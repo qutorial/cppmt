c1d59de846c387dfc3b6f3d37dfd5c4b35c0e2bb
Qutorial <my@ema.il>
2013-08-20 13:31:00 +0200

    Final iteration made to finish each section, Hurrahhh

---
+++ listings/strings.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main( ) 
+{
+   string s1 ( "Blue" );
+   string s2 ( "berry" );
+   cout << "The string concatenation is: " << s1 + s2 << endl;
+}
