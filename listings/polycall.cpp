ec4110928033c0a47b2e8883fdf090d4e23a231b
Qutorial <my@ema.il>
2013-09-01 20:28:05 +0200

    Only bibliography stays

--- listings/polycall.cpp
+++ listings/polycall.cpp
@@ -3,14 +3,17 @@ class Shape {};
 class Circle: public Shape {};
 
 void draw(Shape* shape){
-  // ...
+  // Some drawing implementation, 
+  // delegating, perhaps, to the object.
 }
 
 int main(){
 
   Circle* c = new Circle();
   
-  // Call with a Circle* parameter instead of the declared Shape*
+  // A call bellow happens
+  // with a Circle* type instead of 
+  // the declared Shape* type.
   draw(c);
   
   return 0;
